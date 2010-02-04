657dcd982d45c716f6ce6fed6588c14ade28a692
Joe Stump <joe@joestump.net>
2010-02-03 17:31:16 -0800

    Added some initial code for sending requests to the API.

--- Services/SimpleGeo.php
+++ Services/SimpleGeo.php
@@ -1,14 +1,19 @@
 <?php
 
 require_once 'HTTP/OAuth.php';
+require_once 'Services/SimpleGeo/Exception.php';
 
 class Services_SimpleGeo 
 {
     /**
+     */
+    private $api = 'http://api.simplegeo.com';
+
+    /**
      * OAuth client
      *
      * @var object $oauth Instance of OAuth client
-     * @see HTTP_OAuth
+     * @see HTTP_OAuth_Consumer
      */
     private $oauth = null;
 
@@ -19,11 +24,46 @@ class Services_SimpleGeo
      * @var string $secret Your OAuth secret
      *
      * @return void
-     * @see HTTP_OAuth
+     * @see HTTP_OAuth_Consumer
      */
     public function __construct($token, $secret)
     {
-        $this->oauth = new HTTP_OAuth($token, $secret);
+        $this->oauth = new HTTP_OAuth_Consumer($token, $secret);
+    }
+
+    /**
+     * Reverse geocode a lat/lon to an address
+     *
+     * @var float $lat Latitude
+     * @var float $lon Longitude
+     *
+     * @return mixed
+     */
+    public function getAddress($lat, $lon)
+    {
+        try {
+            $result = $this->oauth->sendRequest('/nearby/address/' . $lat .
+                ',' . $lon);
+        } catch (HTTP_OAuth_Exception $e) {
+            throw new Services_SimpleGeo_Exception($e->getMessage());
+        }
+    }
+
+
+    /**
+     * Send a request to the API
+     *
+     * @var string $endpoint Relative path to endpoint
+     * @var array  $args     Additional arguments passed to HTTP_OAuth
+     * @var string $method   HTTP method to use
+     * 
+     * @return mixed
+     * @see HTTP_OAuth_Consumer::sendRequest()
+     */
+    private function _sendRequest($endpoint, $args = array(), $method = 'GET')
+    {
+        $url = $this->api . $endpoint;
+        return $this->oauth->sendRequest($url, $args, $method);
     }
 }
 
